d06de37ba6d3095a514474fbf874932e7ef8784e
Yahya Ali <yahyaalia394@gmail.com>
2026-05-06 02:55:41 +0500

    Fix: Updated routes

--- backend/index.js
+++ backend/index.js
@@ -5,22 +5,26 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
-import categoryRoutes from "./router/categoryRoute.js";
-import brandRoutes from "./router/brandRoute.js";
-import productRoutes from "./router/productRoute.js";
-import authuser from "./router/userRouter.js";
-import staff from "./router/Staff.router.js";
+import categoryRoutes from "../router/categoryRoute.js";
+import brandRoutes from "../router/brandRoute.js";
+import productRoutes from "../router/productRoute.js";
+import authuser from "../router/userRouter.js";
+import staff from "../router/Staff.router.js";
 
 const app = express();
 
 app.use(cookieParser());
-app.use(cors({ origin: true, credentials: true }));
+
+app.use(cors({
+  origin: true,
+  credentials: true
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.send("Backend running");
+  res.json({ status: "Backend running" });
 });
 
 app.use("/api/auth", authuser);
@@ -28,5 +32,11 @@ app.use("/api/products", productRoutes);
 app.use("/api/staff", staff);
 app.use("/api/categories", categoryRoutes);
 app.use("/api/brands", brandRoutes);
+
+// global error handler (IMPORTANT)
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).json({ message: "Internal Server Error" + err.message });
+});
 
 export default app;
