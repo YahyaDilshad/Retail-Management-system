aa818e184a649c29f415a0071d1e28ac25778a79
Yahya Ali <yahyaalia394@gmail.com>
2026-05-06 01:18:21 +0500

    update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,57 +1,32 @@
-    import dotenv from "dotenv";
-    import path from 'path';
-    import { fileURLToPath } from 'url';
-    import http from 'http'
+import dotenv from "dotenv";
+dotenv.config();
 
-    dotenv.config(); 
-//    import { initWebSocketServer } from "./config/Websocket.server.js";
-  //  import { handleWebsocketConnection } from "./handler/websocket.handler.js";
-    import { sequelize } from "./config/sql.connnect.js";
-    import "./models/index.js";
-    
-    import express from "express";
-    const app = express()
-    import cookieParser from 'cookie-parser'
-    import cors from "cors";
-  
-    
-    import categoryRoutes from './router/categoryRoute.js'
-    import brandRoutes from "./router/brandRoute.js";              
-    import productRoutes from "./router/productRoute.js";
-    import authuser from './router/userRouter.js'
-    import staff from './router/Staff.router.js'
-  
-    // create server with http module
-    const Server = http.createServer(app)
-    
-    
-    
-    // const wss = initWebSocketServer(Server)
-    // wss.on("connection" , ()=> {
-    // handleWebsocketConnection(ws, req, wss)
-    // })
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
-    app.use(cookieParser())
-    app.use(cors({
-      origin: true,
-      credentials: true
-    }));
+import categoryRoutes from "./router/categoryRoute.js";
+import brandRoutes from "./router/brandRoute.js";
+import productRoutes from "./router/productRoute.js";
+import authuser from "./router/userRouter.js";
+import staff from "./router/Staff.router.js";
 
-    app.use(express.json());
-    app.use(express.urlencoded({ extended: true })); //
+const app = express();
 
-    app.use("/api/auth", authuser);
-    app.use("/api/products", productRoutes);
-    app.use("/api/staff" , staff)
-    app.use("/api/categories", categoryRoutes);
-    app.use("/api/brands", brandRoutes);
+app.use(cookieParser());
+app.use(cors({ origin: true, credentials: true }));
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-const PORT = process.env.PORT || 5000;
-    
+app.get("/", (req, res) => {
+  res.send("Backend running");
+});
 
+app.use("/api/auth", authuser);
+app.use("/api/products", productRoutes);
+app.use("/api/staff", staff);
+app.use("/api/categories", categoryRoutes);
+app.use("/api/brands", brandRoutes);
 
-
-export default app
-
-
+export default app;
