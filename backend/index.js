143853268f3b5dbdf0d1d2fcab40a4bb48acb5e9
Yahya Ali <yahyaalia394@gmail.com>
2026-05-06 10:47:28 +0500

    updated index

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(cookieParser());
 
 // Production mein origin ko broad rakhne ke bajaye frontend URL dena behtar hai
 app.use(cors({
-  origin:"https://retail-management-system-imno.vercel.app/", 
+  origin:"https://retail-management-system-imno.vercel.app", 
   credentials: true
 }));
 
