fcaacdf3510779a496a8c0bd33b8328b4aed5fcb
Yahya Ali <yahyaalia394@gmail.com>
2026-05-07 10:07:21 +0500

    Updating sideBard

--- Dashboard_Retail_Management/src/components/sideBar.jsx
+++ Dashboard_Retail_Management/src/components/sideBar.jsx
@@ -34,28 +34,28 @@ const Sidebar = () => {
 
   }
   return (
-    <aside className="w-60 bg-[#0e6d65] text-white h-screen flex flex-col fixed left-0 top-0">
+    <aside className="w-60 bg-[#0e6d65] text-white lg:h-screen flex flex-col fixed left-0 top-0">
       {/* Logo Section */}
       <Link to='/admin' className="flex items-center justify-center gap-2 px-5 mt-5">
         <div className="bg-[#ffffff4d] p-2 rounded-lg">
           <LayoutDashboard   size={24} />
         </div>
-  <h1 className="text-md  text-white">Apexiums Retail Management</h1>
+  <h1 className="text-10  text-white">Apexiums Retail Management</h1>
       </Link>
 
       {/* Menu Links */}
       <nav className="flex-1 px-3 py-6 space-y-2">
         
           <Link
-           className={`flex items-center  text-white hover:bg-[#ffffff4d] gap-2  px-4 py-2 rounded-lg transition-all`}
+           className={`flex items-center  text-white hover:bg-[#ffffff4d] gap-2  px-4 py-1.5 rounded-lg transition-all`}
            to="/admin/dashboard"
           >
             <span><BarChart3/></span>
-            <span>Dashboard</span>
+            <span className="text-10">Dashboard</span>
           </Link>
         
           <Link
-           className={`flex items-center  text-white hover:bg-[#ffffff4d]    gap-3 px-4 py-2 rounded-lg transition-all`}
+           className={`flex items-center  text-white hover:bg-[#ffffff4d]    gap-3 px-4 py-1.5 rounded-lg transition-all`}
            to="/admin/products"
           >   
             <span><Package2/></span>
@@ -63,105 +63,106 @@ const Sidebar = () => {
           </Link>
         
           <Link
-           className={`flex items-center  text-white hover:bg-[#ffffff4d]    gap-3 px-4 py-2 rounded-lg transition-all`}
+           className={`flex items-center  text-white hover:bg-[#ffffff4d]    gap-3 px-4 py-1.5 rounded-lg transition-all`}
            to="/admin/categories"
           >
             <span><Tags/></span>
-            <span>Categories</span>
+            <span  className="text-10">Categories</span>
           </Link>
         
           <Link
-           className={`flex items-center  text-white hover:bg-[#ffffff4d]    gap-3 px-4 py-2 rounded-lg transition-all`}
+           className={`flex items-center  text-white hover:bg-[#ffffff4d]    gap-3 px-4 py-1.5 rounded-lg transition-all`}
            to="/admin/Scanner"  
           >
             <span><ScanLine/></span>
-            <span>Scanner</span>
+            <span  className="text-10">Scanner</span>
           </Link>
         
           <Link
-           className={`flex items-center  text-white hover:bg-[#ffffff4d]    gap-3 px-4 py-2 rounded-lg transition-all`}
+           className={`flex items-center  text-white hover:bg-[#ffffff4d]    gap-3 px-4 py-1.5 rounded-lg transition-all`}
            to='/admin/billing'
           >
             <span><ReceiptCent/></span>
-            <span>Billing</span>
+            <span  className="text-10">Billing</span>
           </Link>
         
           <Link
-           className={`flex items-center text-white hover:bg-[#ffffff4d]  gap-3 px-4 py-2 rounded-lg transition-all`}
+           className={`flex items-center text-white hover:bg-[#ffffff4d]  gap-3 px-4 py-1.5  rounded-lg transition-all`}
            to="/admin/stock-management"
           >
             <span><Boxes/></span>
-            <span>Stock Management</span>
+            <span  className="text-10">Stock Management</span>
           </Link>
         
           <Link
-           className={`flex items-center text-white hover:bg-[#ffffff4d]  gap-3 px-4 py-2 rounded-lg transition-all`}
+           className={`flex items-center text-white hover:bg-[#ffffff4d]  gap-3 px-4 py-1.5  rounded-lg transition-all`}
            to='/admin/staff'
           > 
             <span><Users/></span>
-            <span>Staff</span>
+            <span  className="text-10">Staff</span>
           </Link>
           <Link
-           className={`flex items-center text-white hover:bg-[#ffffff4d]  gap-3 px-4 py-2 rounded-lg transition-all`}
+           className={`flex items-center text-white hover:bg-[#ffffff4d]  gap-3 px-4 py-1.5  rounded-lg transition-all`}
            to='/admin/notification'
           > 
             <span><Bell/></span>
-            <span>Notifications</span>
+            <span  className="text-10">Notifications</span>
           </Link>
           <Link
-           className={`flex items-center text-white hover:bg-[#ffffff4d]  gap-3 px-4 py-2 rounded-lg transition-all`}
+           className={`flex items-center text-white hover:bg-[#ffffff4d]  gap-3 px-4 py-1.5  rounded-lg transition-all`}
            to='/admin/agencies'
           > 
             <span><Building2/></span>
-            <span>Agencies</span>
+            <span  className="text-10">Agencies</span>
           </Link>
           <Link
-           className={`flex items-center text-white hover:bg-[#ffffff4d]  gap-3 px-4 py-2 rounded-lg transition-all`}
+           className={`flex items-center text-white hover:bg-[#ffffff4d]  gap-3 px-4 py-1.5  rounded-lg transition-all`}
            to='/admin/Clients'
           > 
             <span><UserCheck/></span>
-            <span>Clients</span>
+            <span  className="text-10">Clients</span>
           </Link>
           <Link
-           className={`flex items-center text-white hover:bg-[#ffffff4d]  gap-3 px-4 py-2 rounded-lg transition-all`}
+           className={`flex items-center text-white hover:bg-[#ffffff4d]  gap-3 px-4 py-1.5  rounded-lg transition-all`}
            to='/admin/Revenue'
           > 
             <span><DollarSign/></span>
-            <span>Revenue</span>
+            <span  className="text-10">Revenue</span>
           </Link>
           <Link
-           className={`flex items-center text-white hover:bg-[#ffffff4d]  gap-3 px-4 py-2 rounded-lg transition-all`}
+           className={`flex items-center text-white hover:bg-[#ffffff4d]  gap-3 px-4 py-1.5  rounded-lg transition-all`}
            to='/admin/staff-attendance'
           > 
             <span><CalendarCheck/></span>
-            <span>Staf Attendance</span>
+            <span  className="text-10">Staf Attendance</span>
           </Link>
           <Link
-           className={`flex items-center text-white hover:bg-[#ffffff4d]  gap-3 px-4 py-2 rounded-lg transition-all`}
+           className={`flex items-center text-white hover:bg-[#ffffff4d]  gap-3 px-4 py-1.5  rounded-lg transition-all`}
            to='/admin/about-us'
           > 
             <span><Info/></span>
-            <span>About Us</span>
+            <span  className="text-10">About Us</span>
           </Link>
         
           <Link
           onClick={handleAllusers}
-           className={`flex items-center text-white hover:bg-[#ffffff4d] gap-3 px-4 py-2 rounded-lg transition-all`}
+           className={`flex items-center text-white hover:bg-[#ffffff4d] gap-3 px-4 py-1.5 rounded-lg transition-all`}
            to='/admin/all-users'
           >
             <span><User /></span>
-            <span>All Users</span>
+            <span  className="text-10">All Users</span>
+          </Link>
+          <Link
+          onClick={handleAllusers}
+           className={`flex items-center text-white hover:bg-[#ffffff4d] gap-3 px-4 py-1.5 rounded-lg transition-all`}
+           to='/admin/all-users'
+          >
+            <span><LogOut/>
+          </span>
+            <span  className="text-10">Sign Up</span>
           </Link>
     
       </nav>
-
-      {/* Logout Button */}
-      <div className=" h-13 border-t border-white">
-        <button className="flex items-center gap-3 w-full  px-4 py-2 text-white hover:bg-[#ffffff4d]  transition-all">
-          <LogOut size={20} />
-          <span>Sign Out</span>
-        </button>
-      </div>
     </aside>
   );
 };
