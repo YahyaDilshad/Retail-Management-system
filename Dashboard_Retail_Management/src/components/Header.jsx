72004638372f01ba0149f725f79b3e63faf8b287
Yahya Ali <yahyaalia394@gmail.com>
2026-05-11 16:05:47 +0500

    Update code

--- Dashboard_Retail_Management/src/components/Header.jsx
+++ Dashboard_Retail_Management/src/components/Header.jsx
@@ -25,21 +25,26 @@ const result = useQuery({
 
 const notificationData = result.data || [];
   return (
-    <header className="bg-[#F8FAFC] w-[83vw] ml-60">
-      <div className="max-w-7xl mx-auto px-4 py-3  transition-all ease-in-out duration-300 flex items-center justify-end">
+    <header className="bg-[#F8FAFC] w-[80vw] ml-60">
+      <div className="max-w-7xl mx-auto px-4 py-3  flex items-center justify-end">
         <div className="relative right-0 ">
           <button
             onClick={() => setShowMenu(!showMenu)}
             className="flex items-center gap-2 focus:outline-none"
-          > <Bell className="text-2xl text-black" />
+          > <Bell className="text-2xl text-black cursor-pointer" />
           </button>
 
           {showMenu && (
-            <div className="absolute z-99 overflow-y-auto right-0 mt-2 w-100 p-3 h-70 bg-white border border-gray-200 rounded-lg shadow-lg py-2">
+            <div className="absolute z-99 overflow-y-auto transition-all ease-in duration-300 right-0 mt-2 w-100 h-70 bg-white border border-gray-200 rounded-lg shadow-lg py-2">
+                  <div className="border-b-2 border-gray-300 bg-[#F8FAFC] hover: mb-2 p-3 flex items-center justify-between">
+                    <h1 className="text-xl text-gray-400 ">Notification</h1>
+                    <p className="text-sm text-[#20B0A4] font-medium cursor-pointer ">Clear All</p>
+                  </div>
               {notificationData.map((notification) => (
-                <div key={notification.id} className="rounded-xl px-4 py-2 text-sm text-gray-700 hover:bg-gray-200">
-                  <h3>{notification.Title}</h3>
-                  <p>{notification.Message}</p>
+                <div key={notification.id} className=" px-4 py-2 text-sm border-b border-gray-300 text-gray-700 hover:bg-[#ebeff3]">
+                  <h3 className="text-md mb-2">{notification.Title}</h3>
+                  <p className="text-md mb-2" >{notification.Message}</p>
+                  <p>{notification.createdAt}</p>
                 </div>
               ))}
                <button
@@ -47,7 +52,7 @@ const notificationData = result.data || [];
                   setShowMenu(false);
                   navigate("/admin/profile");
                 }}
-                className="w-fit fixed  text-sm text-black hover:border-b-1 hover:border-gray-300 cursor-pinter"
+                className="w-fit fixed  text-sm text-black hover:border-b hover:border-gray-300 cursor-pinter"
               >
                 View All
               </button>
