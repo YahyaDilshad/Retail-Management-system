09b5899fe3d7ff2fba9f766a2ea020b3ffaff0b3
Yahya Ali <yahyaalia394@gmail.com>
2026-08-21 15:59:59 +0500

    adding some file in Dashboard and bug fixing

--- backend/index.js
+++ backend/index.js
@@ -14,14 +14,14 @@ import staff from "./router/Staff.router.js";
 const app = express();
 
 app.use(cookieParser());
-// app.use(cors({
-//   origin : "https://retail-management-system-imno.vercel.app",
-//   credentials : true
-// }))
-  app.use(cors({
-    origin: "http://localhost:5173",
-    credentials : true
-  }))
+app.use(cors({
+  origin : "https://retail-management-system-imno.vercel.app",
+  credentials : true
+}))
+  // app.use(cors({
+  //   origin: "http://localhost:5173",
+  //   credentials : true
+  // }))
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
