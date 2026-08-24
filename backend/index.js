716aac51ef59f2d0b96a057bdbc2761be1f26b43
Yahya Ali <yahyaalia394@gmail.com>
2026-08-24 12:30:19 +0500

    big change

--- backend/index.js
+++ backend/index.js
@@ -20,14 +20,14 @@ import customerRouter from "./router/customerRoute.js"
 const app = express();
 
 app.use(cookieParser());
-// app.use(cors({
-//   origin : "https://retail-management-system-imno.vercel.app",
-//   credentials : true
-// }))
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin : "https://retail-management-system-imno.vercel.app",
   credentials : true
 }))
+// app.use(cors({
+//   origin: "http://localhost:5173",
+//   credentials : true
+// }))
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
